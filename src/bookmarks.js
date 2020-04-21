b9af7fb67dbaec46163217d9633de4f045d4a8c1
mteachey <melindateachey@gmail.com>
2020-04-21 16:57:16 -0600

    initial files set up, route handlers for GET(s) and POST

---
+++ src/bookmarks.js
@@ -0,0 +1,17 @@
+const bookmarks = [{
+    "description":"Where we find everything else",
+    "id":"cjozyzeqh0001lxygb8mhnvhz",
+    "rating":"4",
+    "title":"Google",
+    "url":"https://www.google.com"
+},
+{
+    "description":"The only place to find web documentation",
+    "id":"cjkzyzeqh0001lxygb8mhqvh3",
+    "rating":"5",
+    "title":"MDN",
+    "url":"https://developer.mozilla.org"
+}
+]
+
+module.exports = { bookmarks }
---
+++ src/bookmarks/bookmarks-router.js
@@ -0,0 +1,86 @@
+const express = require('express');
+
+const bookmarkRouter = express.Router();
+const bodyParser = express.json();
+const { v4: uuid} = require('uuid');
+const logger = require('../logger');
+const { bookmarks } = require('../bookmarks.js')
+
+bookmarkRouter
+    .route('/bookmarks')
+    .get((req, res)=>{
+        res
+        .json(bookmarks);
+    })
+    .post(bodyParser, (req, res)=>{
+       const {description, rating, title, url } = req.body;
+
+       if(!description){
+           logger.error(`Description is required`);
+           return res
+            .status(400)
+            .send('Invalid data');
+       }
+       if(!rating){
+        logger.error(`Rating is required`);
+        return res
+         .status(400)
+         .send('Invalid data');
+        }
+        if(!title){
+            logger.error(`Title is required`);
+            return res
+            .status(400)
+            .send('Invalid data');
+        }
+
+        if(!url){
+            logger.error(`URL is required`);
+            return res
+             .status(400)
+             .send('Invalid data');
+        }
+
+        //generate ID and push bookmark array
+        const id = uuid();
+
+        const bookmark ={
+            id,
+            description, 
+            rating,
+            title,
+            url,
+        }
+
+        bookmarks.push(bookmark);
+
+        //log creation and send response
+        logger.info(`Bookmark with id ${id} created`);
+        res
+            .status(201)
+            .location(`http://localhose:8000/bookmarks/${id}`)
+            .json(bookmark);
+
+    })//end route POST /bookmarks
+
+bookmarkRouter
+    .route('/bookmarks/:id')
+    .get((req, res)=>{
+        const { id } = req.params;
+        const bookmark = bookmarks.find(b =>b.id == id);
+
+        if(!bookmark){
+            logger.error(`Bookmark with id ${id} not found`);
+            return res
+                .status(404)
+                .send('Bookmark Not Found');
+        }
+
+        res.json(bookmark);
+
+    })
+    .delete((req, res)=>{
+
+    })
+
+module.exports=bookmarkRouter;
