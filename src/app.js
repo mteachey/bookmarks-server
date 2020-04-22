850bad011ac33b9641611118d466a942e5493ee8
mteachey <melindateachey@gmail.com>
2020-04-22 08:20:33 -0600

    fixed missing error message

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const logger = require('./logger');
 const bookmarkRouter = require('./bookmarks/bookmarks-router.js');
 
 const app = express()
